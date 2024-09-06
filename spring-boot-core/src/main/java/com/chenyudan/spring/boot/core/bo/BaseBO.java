d7239fd0baa35c88a14fa675334de35ebc09995e
chenyu <yu.chen@narwal.com>
2024-09-06 14:41:15 +0800

    init

---
+++ spring-boot-core/src/main/java/com/chenyudan/spring/boot/core/bo/BaseBO.java
@@ -0,0 +1,14 @@
+package com.chenyudan.spring.boot.core.bo;
+
+import lombok.Data;
+
+/**
+ * Description: TODO
+ *
+ * @author chenyu
+ * @since 2024/9/6 14:40
+ */
+@Data
+public class BaseBO {
+
+}
---
+++ spring-boot-core/src/main/java/com/chenyudan/spring/boot/core/utils/StringUtil.java
@@ -0,0 +1,12 @@
+package com.chenyudan.spring.boot.core.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Description: TODO
+ *
+ * @author chenyu
+ * @since 2024/9/6 14:40
+ */
+public class StringUtil extends StringUtils {
+}
