011be0a561065582759b61a40702bbc4ffbe8579
chenyu <yu.chen@narwal.com>
2024-10-15 10:16:28 +0800

    http工具

--- spring-boot-core/src/main/java/com/chenyudan/spring/boot/core/utils/HttpUtil.java
+++ spring-boot-core/src/main/java/com/chenyudan/spring/boot/core/utils/HttpUtil.java
@@ -1,18 +1,17 @@
 package com.chenyudan.spring.boot.core.utils;
 
-import com.chenyudan.spring.boot.core.error.BaseError;
-import com.chenyudan.spring.boot.domain.exception.BaseException;
+import com.google.gson.GsonBuilder;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.Headers;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Request.Builder;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import okhttp3.ResponseBody;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -25,60 +24,37 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class HttpUtil {
 
-    private static final OkHttpClient httpClient = new OkHttpClient().newBuilder().connectTimeout(10L, TimeUnit.SECONDS).callTimeout(30L, TimeUnit.SECONDS).build();
+    private static final String APPLICATION_JSON_CHARSET_UTF_8 = "application/json; charset=utf-8";
 
-    public static String get(String url, Map<String, Object> params) {
-        return get(url, null, params);
+    private final OkHttpClient okHttpClient;
+
+    HttpUtil(OkHttpClient okHttpClient) {
+        this.okHttpClient = okHttpClient;
     }
 
-    public static String postForJson(String url, Map<String, Object> params, String body) {
-        return postForJson(url, null, params, body);
+    public static HttpUtil create(Long connectTimeout, Long callTimeout) {
+        OkHttpClient httpClient = new OkHttpClient().newBuilder().connectTimeout(connectTimeout, TimeUnit.SECONDS).callTimeout(callTimeout, TimeUnit.SECONDS).build();
+        return new HttpUtil(httpClient);
     }
 
-    public static String get(String url, Map<String, String> headers, Map<String, Object> params) {
-        String actualUrl = buildUrl(url, params);
-        Request.Builder builder = new Request.Builder().url(actualUrl).get();
-        if (headers != null) {
-            headers.forEach(builder::addHeader);
-        }
-        return getResult(builder);
+    public Response get(String url) throws IOException {
+        return this.get(url, new HashMap<>());
     }
 
-    public static String postForJson(String url, Map<String, String> headers, Map<String, Object> params, String body) {
-        String actualUrl = buildUrl(url, params);
-        MediaType mediaType = MediaType.get("application/json");
-        RequestBody requestBody = RequestBody.create(mediaType, body);
-        Request.Builder builder = new Request.Builder().url(actualUrl).post(requestBody);
-        if (headers != null) {
-            headers.forEach(builder::addHeader);
-        }
-        return getResult(builder);
+    public Response get(String url, Map<String, String> headerMap) throws IOException {
+        Headers headers = Headers.of(headerMap);
+        Request request = new Builder().url(url).get().headers(headers).build();
+        return okHttpClient.newCall(request).execute();
     }
 
-    private static String getResult(Builder builder) {
-        try (Response response = httpClient.newCall(builder.build()).execute()) {
-            AssertUtil.isTrue(response.isSuccessful(), BaseError.CONNECT_ERROR);
-            ResponseBody responseBody = response.body();
-            return responseBody == null ? null : responseBody.string();
-        } catch (IOException e) {
-            throw new BaseException(BaseError.CONNECT_ERROR);
-        }
+    public Response postForJson(String url, Object body) throws IOException {
+        return this.postForJson(url, body, new HashMap<>());
     }
 
-    /**
-     * 构建完整请求地址
-     *
-     * @param url    地址
-     * @param params 参数
-     */
-    public static String buildUrl(String url, Map<String, Object> params) {
-        String actualUrl = url;
-        if (params != null && !params.isEmpty()) {
-            List<String> paramList = LambdaUtil.mapToList(params, entry -> entry.getKey() + "=" + entry.getValue());
-            String paramsStr = StringUtil.join(paramList, "&");
-            actualUrl = actualUrl + "?" + paramsStr;
-        }
-        log.debug("http请求地址:{}", actualUrl);
-        return actualUrl;
+    public Response postForJson(String url, Object body, Map<String, String> headerMap) throws IOException {
+        Headers headers = Headers.of(headerMap);
+        RequestBody requestBody = RequestBody.create(MediaType.get(APPLICATION_JSON_CHARSET_UTF_8), new GsonBuilder().create().toJson(body));
+        Request request = new Request.Builder().url(url).post(requestBody).headers(headers).build();
+        return okHttpClient.newCall(request).execute();
     }
 }
